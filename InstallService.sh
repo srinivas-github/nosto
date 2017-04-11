0ab57f87b444e4c1f9a4561e3a962759170e09ca
Jillala Srinivas Reddy <srinivasreddy.jillala@teradata.com>
2017-04-11 13:19:25 +0530

    Added Mysql Installation from source

--- InstallService.sh
+++ InstallService.sh
@@ -71,11 +71,58 @@ install_apache2_package()
 
 install_mysql_source()
 {
-    
+    #get the source
+    SrcDir="MySQLSrc"
+    [[ -d $SrcDir ]] || mkdir $SrcDir
+
+    cd ./$SrcDir
+ 
+    #Preconfiguration steps
+    groupadd mysql
+    useradd -r -g mysql -s /bin/false mysql
+
+
+    apt-get source mysql-5.5
+    # Get the source directory
+    mysql_src=$(find . -mindepth 1 -maxdepth 1 -type d)
+    cd $mysql_src
+
+    if [ $? -eq 0 ]; then
+        mkdir bld
+	cd bld
+	cmake ..
+	if [ $? eq 0 ]; then
+	    echo -e "\n cmake successful..\n"
+	    make
+	    if [ $? -eq 0 ]; then
+	        echo -e "\n make successful..\n"
+		sudo make install 
+		if [ $? -eq 0 ]; then
+		    echo -e "\n make install successful..\n"
+		fi
+	    fi
+	fi
+    fi
+
+    #post installation 
+    cd /usr/local/mysql
+    chown -R mysql .
+    chgrp -R mysql .
+    scripts/mysql_install_db --user=mysql
+    chown -R root .
+    chown -R mysql data
+
 }
 
 install_apache_source()
 {
+    # get the source package
+    mkdir -p Apache2Src
+    cd Apache2Src
+    apt-get source apache2
+
+    # get the 
+
 }
 
 ##Main Starts here
